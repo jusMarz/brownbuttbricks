a397c5295b68015ebc39abfec160d52f2595bd5d
BT_2S14_14 <memail@seemail>
2025-04-02 10:12:07 -0400

    Second push (of poop)

--- src/BrickLayout.java
+++ src/BrickLayout.java
@@ -97,4 +97,8 @@ public class BrickLayout {
             return false;
         }
     }
+
+    public int[][] getBrickLayout() {
+        return brickLayout;
+    }
 }
--- src/TestBrick.java
+++ src/TestBrick.java
@@ -1,6 +1,8 @@
 public class TestBrick {
     public static void main(String[] args) {
-        BrickLayout b = new BrickLayout("src/bricks", 7, true);
+        BrickLayout b = new BrickLayout("src/bricks", 39, false);
+        b.printBrickLayout();
+        b.doOneBrick();
         b.printBrickLayout();
     }
 }
